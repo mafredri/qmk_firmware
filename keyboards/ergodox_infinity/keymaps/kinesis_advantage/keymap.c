39bbc13a8a7dba427e4f9da1ed53bc5460956bf5
Mathias Fredriksson <mafredri@gmail.com>
2019-04-22 13:28:27 +0300

    ergodox_infinity: Enable programming with either half

--- keyboards/ergodox_infinity/keymaps/kinesis_advantage/keymap.c
+++ keyboards/ergodox_infinity/keymaps/kinesis_advantage/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap 0: Basic layer
      *
      * ,--------------------------------------------------.      ,--------------------------------------------------.
-     * |   =    |   1  |   2  |   3  |   4  |   5  | Esc  |      |  L3  |   6  |   7  |   8  |   9  |   0  |   -    |
+     * |   =    |   1  |   2  |   3  |   4  |   5  |  L3  |      |  L3  |   6  |   7  |   8  |   9  |   0  |   -    |
      * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
      * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |      |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
      * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Otherwise, it needs KC_*
     [BASE] = LAYOUT_ergodox(
         // left hand
-        KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_ESC,     //
+        KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, MO(PROG),   //
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, MO(SYMB),   //
         LCAG_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G,     //
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, MO(MDIA),  //
@@ -151,7 +151,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,                                                        //
         KC_TRNS, KC_TRNS, KC_TRNS,
         // right hand
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,    //
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           //
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  //
